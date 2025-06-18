f2f2815ec316c2ac0773a2d264d8fa4ac524387d
carljin <buffpal2016@gmail.com>
2025-06-18 15:51:13 -0400

    feat: 支持外部提供语言包

--- src/context/I18nContext.tsx
+++ src/context/I18nContext.tsx
@@ -1,14 +1,15 @@
 import { createContext, ReactNode } from "react";
 import { I18nextProvider } from "react-i18next";
 import i18nInstance from "@/i18n/init";
+import { i18n } from "i18next";
 
 export type I18nContextType = NonNullable<unknown>;
 
 export const I18nContext = createContext<I18nContextType | null>(null);
 
-export function I18nContextCmp(props: { children: ReactNode }) {
+export function I18nContextCmp(props: { children: ReactNode; i18n?: i18n }) {
   return (
-    <I18nextProvider i18n={i18nInstance} defaultNS={"common"}>
+    <I18nextProvider i18n={props.i18n || i18nInstance} defaultNS={"common"}>
       <I18nContext.Provider value={{}}>{props.children}</I18nContext.Provider>
     </I18nextProvider>
   );
